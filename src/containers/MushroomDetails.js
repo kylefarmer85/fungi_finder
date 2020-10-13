de7a0bcfaaa459421301c7ff446bb7cd41334885
NMoore42 <nicholas.moore5@icloud.com>
2020-10-13 14:18:10 -0500

    starter code

--- src/containers/MushroomDetails.js
+++ src/containers/MushroomDetails.js
@@ -2,18 +2,12 @@ import React from 'react'
 
 class MushroomDetails extends React.Component {
 
-  renderDetails = () => {
-    if (this.props.mushroomDetail.details){
-      return <p>{this.props.mushroomDetail.details}</p>
-    } else {
-      return <h1 style={{textAlign: "center"}}>Welcome to the Mushroom page!</h1>
-    }
-  }
+
 
   render(){
     return(
       <div className="ui segment inverted blue mush-details">
-        {this.renderDetails()}
+
       </div>
     )
   }
